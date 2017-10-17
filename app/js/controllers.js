1d95d8b343a1873c79837588b89671755dfcc97b
Nixon Sette <nixonsette@gmail.com>
2017-10-17 21:55:44 +0000

    Controller Criado

---
+++ app/js/controllers.js
@@ -0,0 +1,25 @@
+var app = angular.module('ecommerce', ['ngRoute'])
+
+.config(function($routeProvider){
+    $routeProvider
+    
+    .when("/", {
+        templateUrl: 'app/pages/bemvindo.html',
+        controller: 'BemVindoController'
+    })
+    
+    .otherwise({
+       redirectTo: '/' 
+    });
+})
+
+.controller('BemVindoController', function($scope, $http){
+    $scope.listaCategorias = {};
+    
+    $scope.init = function(){
+        $http.get("/api/categoria/consultar.php?retornar_imagem=sim")
+            .then(function(data){
+               $scope.listaCategorias = data.data; 
+            });
+    }
+})
